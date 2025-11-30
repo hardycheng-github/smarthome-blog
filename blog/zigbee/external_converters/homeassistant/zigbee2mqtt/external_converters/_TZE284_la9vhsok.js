8987eff6ddbac97fdaa85c8a2bd45f78c4e0373b
hardycheng-github <55072235+hardycheng-github@users.noreply.github.com>
2025-11-30 14:49:38 +0800

    Create _TZE284_la9vhsok.js

---
+++ blog/zigbee/external_converters/homeassistant/zigbee2mqtt/external_converters/_TZE284_la9vhsok.js
@@ -0,0 +1,45 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const tuya = require('zigbee-herdsman-converters/lib/tuya');
+const legacy = require('zigbee-herdsman-converters/lib/legacy');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const invertedTuyaCoverControl = {
+    key: ['state', 'position'],
+    convertSet: async (entity, key, value, meta) => {
+        if (key === 'position') {
+            value = 100 - value; // Invert the position value
+        }
+        return await legacy.toZigbee.tuya_cover_control.convertSet(entity, key, value, meta);
+    },
+};
+
+const definition = {
+    zigbeeModel: [],
+    fingerprint: [
+        tuya.fingerprint('TS0601', [
+            '_TZE284_la9vhsok',
+        ]),
+    ],
+    model: 'TS0601_cover_1',
+    vendor: 'Tuya',
+    description: 'Curtain motor with corrected percentage setting',
+    whiteLabel: [],
+    fromZigbee: [legacy.fromZigbee.tuya_cover, fz.ignore_basic_report],
+    toZigbee: [invertedTuyaCoverControl, legacy.toZigbee.tuya_cover_options],
+    exposes: [
+        e.cover_position().setAccess('position', ea.STATE_SET),
+        e.composite('options', 'options', ea.STATE_SET)
+            .withFeature(e.numeric('motor_speed', ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(255)
+                .withDescription('Motor speed'))
+            .withFeature(e.binary('reverse_direction', ea.STATE_SET, true, false)
+                .withDescription('Reverse the motor direction')),
+    ],
+};
+
+module.exports = definition;
